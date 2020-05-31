ce9412b18ab05387bb27496b0c38b975f9baec38
jacquelinegavilanes <jigavilanes1@espe.edu.ec>
2020-05-31 18:57:04 -0500

    version1

---
+++ POO_Actividad1_TI.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+void presentarMensaje();
+
+void presentarMensaje() {
+    cout << "" << endl;
+	cout << "Hola Mundo" << endl;
+}
+
+int main() {
+	presentarMensaje();
+	system ("Pause");
+    return 0;
+}
